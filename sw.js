94b7dd4ef5e69a2614a7441a814272b68fbd2df9
nicolekwoh <69892377+nicolekwoh@users.noreply.github.com>
2026-07-17 01:01:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "race-tracker-v6";
+const CACHE = "race-tracker-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
